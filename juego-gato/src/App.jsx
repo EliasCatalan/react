72ce735942f8f9358c932cf4a2cdaf252413db92
Elias Catalán <miguel_catalan_garcia@hotmail.com>
2024-11-18 20:46:09 -0300

    - Termino de juego al encontrar ganador

--- juego-gato/src/App.jsx
+++ juego-gato/src/App.jsx
@@ -22,23 +22,63 @@ const Square = ({ children, isSelected, updateBoard, index }) =>
     )
 }
 
+// Combinaciones ganadoras
+const WINNER_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
 function App() 
 {
-    const [ board, setBoard ] = useState( 
-        Array(9).fill(null) 
-    )
-
-    const [ turn, setTurn ] = useState( TURNS.X )
+    // Manejo de estados
+    const [ board, setBoard ]       = useState( Array(9).fill(null) )
+    const [ turn, setTurn ]         = useState( TURNS.X )
+    const [ winner, setWinner ]     = useState( null )
     
-    const updateBoard = ( index) => 
+    const updateBoard = ( index ) => 
     {
+        // Si la casilla ya está ocupada o hay ganador, no se puede hacer nada
+        if( board[index] || winner ) return
+
+        // Actualizar el tablero
         const newBoard = [...board]
         newBoard[index] = turn
         setBoard( newBoard )
 
+        // Comprobar si hay ganador
+        const newWinner = checkWinner( newBoard )
+        if( newWinner ) {
+            setWinner( newWinner )
+            return
+        }
+
+        // Cambiar el turno
         const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
         setTurn( newTurn )
+    }
+
+    // Comprobar si hay ganador
+    const checkWinner = ( boardToCheck ) => 
+    {
+        for( const combo of WINNER_COMBOS ) 
+        {
+            const [ a, b, c ] = combo
+
+            if( boardToCheck[a] && 
+                boardToCheck[a] === boardToCheck[b] && 
+                boardToCheck[a] === boardToCheck[c] 
+            ) {
+                return boardToCheck[a]
+            }
+        }
+
+        return null
     }
 
     return (
@@ -52,7 +92,8 @@ function App()
                                 key={index} 
                                 index={index} 
                                 updateBoard={updateBoard}
-                            >    
+                            >
+                            { board[index] }
                             </Square>
                         )
                     })
